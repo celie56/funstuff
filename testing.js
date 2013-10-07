90cf8334c23ecfd55584af39d9036b1fab956bca
Chris <celie56@gmail.com>
2013-10-07 15:39:19 -0700

    Just some functions

--- testing.js
+++ testing.js
@@ -1,1 +1,21 @@
-console.log("welcome");
+console.log("Welcome Home");
+
+var input = "undefined"
+
+//helper function that returns the longest string of characters
+function longest_word(strings) {
+    var temp = strings[0];
+    for (n = 0; n < strings.length; n++)
+        if (strings[n].length > temp.length)
+            temp = strings[n];
+    return temp;
+}
+
+//this will find the longest string of characters seperated by spaces
+function longest_sentence(input) {
+    console.log("you entered:", input);
+    var strings = input.split(" ");
+    var word = longest_word(strings);
+    return word;
+}
+
